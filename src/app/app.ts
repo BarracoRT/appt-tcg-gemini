7b95a0912bc79f3b451e27d25d8fdc66fb77f44c
Barraco <lprivera86@outlook.es>
2026-06-02 16:21:49 +0200

    feat: listado de cartas con interaccion y persistencia local

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,40 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('tcg-vault');
+
+  // Listado por defecto (solo se usa la primerísima vez)
+  cartas = [
+    { id: 'OP01-001', name: 'Monkey.D.Luffy', rarity: 'Leader', quantity: 2 },
+    { id: 'OP01-013', name: 'Roronoa Zoro', rarity: 'SR', quantity: 4 },
+    { id: 'OP01-016', name: 'Nami', rarity: 'R', quantity: 3 }
+  ];
+
+  // 🧠 Este bloque se ejecuta solo al cargar la app en el navegador
+  constructor() {
+    if (typeof window !== 'undefined') {
+      const datosGuardados = localStorage.getItem('mis_cartas_tcg');
+      if (datosGuardados) {
+        this.cartas = JSON.parse(datosGuardados); // Recuperamos lo guardado
+      }
+    }
+  }
+
+  sumarCarta(carta: any) {
+    carta.quantity++;
+    this.guardarEnDisko(); // Guardamos el cambio
+  }
+
+  restarCarta(carta: any) {
+    if (carta.quantity > 0) {
+      carta.quantity--;
+      this.guardarEnDisko(); // Guardamos el cambio
+    }
+  }
+
+  // 💾 Función mágica para guardar la lista actual en el navegador
+  guardarEnDisko() {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('mis_cartas_tcg', JSON.stringify(this.cartas));
+    }
+  }
 }
